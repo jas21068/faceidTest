6f1fcac03ba55e8bebd60cbbcacae78fbedd6cea
jas21068 <83102649+jas21068@users.noreply.github.com>
2021-08-17 15:37:32 -0700

    TestFaceId
    TestFaceId

---
+++ faceidTest/faceidTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  faceidTestApp.swift
+//  faceidTest
+//
+//  Created by Jaskirat Mangat on 2021-08-09.
+//
+
+import SwiftUI
+
+@main
+struct faceidTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
